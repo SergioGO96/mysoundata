f4e126c85e304a7645ef3494a370a40c3628c84c
SergioGO96 <sergio.dh.96@gmail.com>
2017-05-03 12:38:40 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -10,4 +10,20 @@ pelicula= raw_input("pelicula: ")
 
 r= requests.get(url_base+pelicula)
 doc = r.json()
-print doc
+if doc["Response"] == "False":
+	print "ERROR"
+else:
+	print "Titulo: " + doc["Title"]
+	print "Estreno: " + doc["Released"]
+	print "Calificacion de edad: " + doc["Rated"]
+	print "Duracion: " + doc["Runtime"]
+	print "Genero: " + doc["Genre"]
+	print "Director: " + doc["Director"]
+	print "Guionistas: " + doc["Writer"]
+	print "Actores: " + doc["Actors"]
+	print "Pais: " + doc["Country"]
+	print "Produccion: " + doc["Production"]
+	print "Pagina Web: " + doc["Website"]
+	print "Valoracion: " + doc["imdbRating"]
+	print "Recaudacion: " + doc["BoxOffice"]
+
