f5d29b3d31a1f70f901150b8317f2759f9233b07
SergioGO96 <sergio.dh.96@gmail.com>
2017-05-05 11:58:47 +0200

    Update principal.py

--- principal.py
+++ principal.py
@@ -32,7 +32,7 @@ def inicio():
 		web = doc["Website"]
 		valoracion = doc["imdbRating"]
 		recaudacion = doc["BoxOffice"]
-		poster = doc["poster"]
+		poster = doc["Poster"]
 	return template('resultado.tpl',titulo=titulo,estreno=estreno,calificacion=calificacion,duracion=duracion,genero=genero,director=director,guionistas=guionistas,actores=actores,pais=pais,Produccion=Produccion,web=web,valoracion=valoracion,recaudacion=recaudacion,poster=poster)
 
 @route('/static/<filepath:path>')
