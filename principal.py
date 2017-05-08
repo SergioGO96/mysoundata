75ab6a9ab272c9c3071d36eb489e4d1de9604768
SergioGO96 <sergio.dh.96@gmail.com>
2017-05-08 10:18:10 +0200

    Update principal.py

--- principal.py
+++ principal.py
@@ -36,7 +36,7 @@ def inicio():
 			return template('resultadosin.tpl',titulo=titulo,estreno=estreno,calificacion=calificacion,duracion=duracion,genero=genero,director=director,guionistas=guionistas,actores=actores,pais=pais,Produccion=Produccion,web=web,valoracion=valoracion,recaudacion=recaudacion,poster=poster)
 		else:
 			poster = doc["Poster"]
-	return template('resultado.tpl',titulo=titulo,estreno=estreno,calificacion=calificacion,duracion=duracion,genero=genero,director=director,guionistas=guionistas,actores=actores,pais=pais,Produccion=Produccion,web=web,valoracion=valoracion,recaudacion=recaudacion,poster=poster)
+			return template('resultado.tpl',titulo=titulo,estreno=estreno,calificacion=calificacion,duracion=duracion,genero=genero,director=director,guionistas=guionistas,actores=actores,pais=pais,Produccion=Produccion,web=web,valoracion=valoracion,recaudacion=recaudacion,poster=poster)
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
