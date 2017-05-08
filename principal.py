4ffe0b51586702a2dd44b357eca9e6724573ad9f
SergioGO96 <sergio.dh.96@gmail.com>
2017-05-08 12:28:45 +0200

    Update principal.py

--- principal.py
+++ principal.py
@@ -3,8 +3,15 @@ from bottle import *
 import requests
 from sys import argv
 from urlparse import parse_qs
+from requests_oauthlib import OAuth1
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import TokenExpiredError
 import json
+
 url_base = 'http://www.omdbapi.com/?t='
+client_id='d9afcb301391465dbc1ea87d231c4dcc'
+client_secret='7657feabd24541688c96c08c6c9098b6'
+token_url = "https://accounts.spotify.com/api/token"
 
 @route('/', method="get")
 def formularioinicio():
@@ -34,6 +41,51 @@ def inicio():
 		else:
 			poster = doc["Poster"]
 			return template('resultado.tpl',titulo=titulo,estreno=estreno,calificacion=calificacion,duracion=duracion,genero=genero,director=director,guionistas=guionistas,actores=actores,pais=pais,valoracion=valoracion,poster=poster)
+		
+def token_valido():
+	token=request.get_cookie("token", secret='some-secret-key')
+  	if token:
+   		 token_ok = True
+   		try:
+      			oauth2 = OAuth2Session(client_id, token=token)
+      			r = oauth2.get('https://www.googleapis.com/oauth2/v1/userinfo')
+      		except TokenExpiredError as e:
+      			token_ok = False
+  	else:
+   		token_ok = False
+  	return token_ok
+@get('/login')
+def LOGIN():
+	if token_valido():
+    		redirect("/listas")
+ 	 else:
+    		response.set_cookie("token", '',max_age=0)
+    		oauth2 = OAuth2Session(client_id, redirect_uri=redirect_uri,scope=scope)
+    		authorization_url, state = oauth2.authorization_url('https://accounts.spotify.com/authorize/')
+    		response.set_cookie("oauth_state", state)
+    		redirect(authorization_url)
+
+@get('/callback_spotify')
+def get_token():
+  	oauth2 = OAuth2Session(client_id, state=request.cookies.oauth_state,redirect_uri=redirect_uri)
+  	token = oauth2.fetch_token(token_url, client_secret=client_secret,authorization_response=request.url)
+ 	response.set_cookie("token", token,secret='some-secret-key')
+  	redirect("/listas")
+
+@get('/listas')
+def personal():
+	token = request.get_cookie("token", secret='some-secret-key')
+	tokens = token["token_type"]+" "+token["access_token"]
+	headers = {"Accept":"aplication/json","Authorization":tokens}
+	perfil = requests.get("https://api.spotify.com/v1/me", headers=headers)
+	if perfil.status_code == 200:
+		cuenta = perfil.json()
+		cuenta = cuenta["id"]
+		url_playlists = "https://api.spotify.com/v1/users/"+str(cuenta)+"/playlists"
+	listas = requests.get(url_playlists, headers=headers)
+	if listas.status_code == 200:
+		playlists_usuario = listas.json()
+	return template('listas.tpl', listas_usuario=playlists_usuario)
 		
 @route('/static/<filepath:path>')
 def server_static(filepath):
