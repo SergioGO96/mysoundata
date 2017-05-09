ceb8bd551e2f0ecdff2a941127290e7a2a8519d2
SergioGO96 <sergio.dh.96@gmail.com>
2017-05-09 14:34:37 +0200

    Update principal.py

--- principal.py
+++ principal.py
@@ -89,7 +89,7 @@ def salir():
 	
 @route('/lista',method='GET')
 def list():
-	listas = request.get(url_playlists)
+	listas = requests.get(url_playlists)
 	if listas.status_code == 200:
 		playlists_usuario = json.loads(listas.text)
 	return template('playlist.tpl',listas=playlists_usuario)
